397b9fd3654b0d1c72b959c4c5193b95d6712f71
Nemanja Maric <nemanjammaric@gmail.com>
2015-07-30 10:04:35 +0200

    Create Static.php

---
+++ Static/Static.php
@@ -0,0 +1,9 @@
+<?php
+
+class staticDB extends Database 
+{
+	protected static $_server = 'localhost';
+	protected static $_database = 'oop_db';
+	protected static $_user = 'oop_user';
+	protected static $_pass = 'pass123';
+}
